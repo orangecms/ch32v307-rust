7cca65ee1c19c7d3e6e2b333da51051e9761f2da
Daniel Maslowski <info@orangecms.org>
2022-11-23 19:11:15 +0100

    IT WORKS sorta, we can build&flash

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,54 @@
-#![feature(naked_functions, asm_sym, asm_const)]
 #![no_std]
 #![no_main]
-use ch32v3::ch32v30x;
+
+use riscv_rt::entry;
 use core::{
     arch::asm,
     panic::PanicInfo,
     ptr::slice_from_raw_parts,
 };
 // use embedded_hal::serial::nb::Write;
-use riscv;
+use panic_halt as _;
+// riscv provides implementation for critical-section
+use riscv as _;
 
-// This is a type alias for the enabled `restore-state-*` feature.
-// For example, it is `bool` if you enable `restore-state-bool`.
-use critical_section::RawRestoreState;
-
-struct MyCriticalSection;
-critical_section::set_impl!(MyCriticalSection);
-
-unsafe impl critical_section::Impl for MyCriticalSection {
-    unsafe fn acquire() -> RawRestoreState {
-        // TODO
-    }
-
-    unsafe fn release(token: RawRestoreState) {
-        // TODO
-    }
-}
+use ch32v3::ch32v30x;
 
 #[no_mangle]
 extern "C" fn DefaultHandler() {}
 
-const STACK_SIZE: usize = 2 * 1024; // 2KiB
-
-#[link_section = ".bss.uninit"]
-static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
-
-/// Set up stack and jump to executable code.
-///
-/// # Safety
-///
-/// Naked function.
-#[naked]
-#[export_name = "start"]
-#[link_section = ".text.entry"]
-pub unsafe extern "C" fn start() -> ! {
-    asm!(
-        // "0:",
-        // "li t4, 0x43",
-        // "li t5, 0x12440000",
-        // "sw t4, 0(t5)",
-        // "j 0b", // debug: CCCCCCCCCCC
-        // Clear feature disable CSR
-        // "csrwi  0x7c1, 0",
-
-        "csrw   mie, zero",
-        "csrw   mstatus, zero",
-        // 2. initialize programming language runtime
-        // clear bss segment
-        "la     t0, _sbss",
-        "la     t1, _ebss",
-        "1:",
-        "bgeu   t0, t1, 1f",
-        "sw     x0, 0(t0)",
-        "addi   t0, t0, 4",
-        "j      1b",
-        "1:",
-        // 3. prepare stack
-        "la     sp, {stack}",
-        "li     t0, {stack_size}",
-        "add    sp, sp, t0",
-        // "j _debug",
-        "call   {main}",
-        stack      =   sym STACK,
-        stack_size = const STACK_SIZE,
-        main       =   sym main,
-        options(noreturn)
-    )
-}
-
-fn main() {
-    let mut peripherals = ch32v30x::Peripherals::take().unwrap();
+#[entry]
+fn main() -> ! {
+    let peripherals = ch32v30x::Peripherals::take().unwrap();
     let gpioa = &peripherals.GPIOA;
     gpioa.outdr.modify(|_, w| w.odr0().set_bit());
+    let gpiob = &peripherals.GPIOB;
 
     // println!("Hello, world!");
-}
+        // HSI 8MHz
+    // 4 opcodes to do a nop sleep here
+    let cycle = 8_000_000 / 4;
+    loop {
+        gpiob.outdr.modify(|_, w| w.odr8().set_bit());
+        for _ in 0..cycle {
+            unsafe {
+                riscv::asm::nop();
+            }
+        }
 
+        gpiob.outdr.modify(|_, w| w.odr8().clear_bit());
+        for _ in 0..cycle {
+            unsafe {
+                riscv::asm::nop();
+            }
+        }
+    }
+}
+/*
 #[cfg_attr(not(test), panic_handler)]
 fn panic(info: &PanicInfo) -> ! {
     loop {
         core::hint::spin_loop();
     }
 }
+*/
